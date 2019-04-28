9ce1239b4c44ea18a8f928e101a8882275779bd3
DaveSholy <48659370+DaveSholy@users.noreply.github.com>
2019-04-29 00:24:57 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,10 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 console.log("Scrpit By Dream");
 
-
 client.on("ready", () => {
-let channel =     client.channels.get("568365914190381056")
+let channel =     client.channels.get("572109782614278154")
 setInterval(function() {
 channel.send(` Made From  before Dave s ض س سث ض ض س`);
 }, 90)
 })
-
 client.login(process.env.BOT_TOKEN);
